415f6ec010ea7401142adc7c81e501b587ecff92
Gokulmurugan1997 <Gokulanshvi@gmail.com>
2024-02-12 16:10:08 +0530

    update PORT

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 const { format } = require("date-fns");
-const PORT = 7000;
+const PORT = process.env.PORT || 7000;
 app.use(cors());
 app.use(express.json());
 
